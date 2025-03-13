69bad7e742569e7ea8bda2ca60f5b758e4dbd9ff
Young <dndud5182@gmail.com>
2025-03-13 15:11:48 +0900

    Feat : 최신 버전 및 아이템 데이터를 가져오는 함수 추가

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,4 +1,5 @@
 import { ChampionData } from "@/types/champion";
+import { ItemData } from "@/types/item";
 
 // 기본 URL (라이엇 API의 데이터 드래곤 서버)
 export const API_BASE_URL = "https://ddragon.leagueoflegends.com";
@@ -30,6 +31,26 @@ export const getChampionData = async (): Promise<ChampionData | null> => {
     return data; // 챔피언 데이터 반환
   } catch (error) {
     console.log("챔피언 데이터를 불러오는 중 오류 발생 : ", error);
+    return null;
+  }
+};
+
+// 아이템 데이터 가져오기
+export const getItemData = async (): Promise<ItemData | null> => {
+  // API 요청 (SSG 적용)
+  const version = await getLatestVersion(); // 최신 버전 정보 가져오기
+  const itemUrl = `${API_BASE_URL}/cdn/${version}/data/ko_KR/item.json`;
+
+  try {
+    const response = await fetch(itemUrl, {
+      cache: "force-cache",
+    });
+
+    const { data } = await response.json();
+
+    return data; // 아이템 데이터 반환
+  } catch (error) {
+    console.log("아이템 데이터를 불러오는 중 오류 발생 : ", error);
     return null;
   }
 };
