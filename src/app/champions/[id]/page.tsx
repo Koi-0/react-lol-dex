3c4c96fe9d99e9314e62a6f91871b551ea427b8c
Young <dndud5182@gmail.com>
2025-03-13 22:15:10 +0900

    Feat : 챔피언 상세 데이터 로딩 및 화면에 표시 기능 구현

---
+++ src/app/champions/[id]/page.tsx
@@ -0,0 +1,47 @@
+import { API_BASE_URL, getChampionDetailData } from "@/utils/serverApi";
+import Image from "next/image";
+
+interface DetailIdProps {
+  params: Promise<{ id: string }>;
+}
+
+const ChampionDetailIdPage = async ({ params }: DetailIdProps) => {
+  const { id } = await params;
+  const championsData = await getChampionDetailData(id);
+
+  if (!championsData) {
+    return <p>챔피언 상세 데이터를 불러오는 데 실패했습니다.</p>;
+  }
+
+  return (
+    <div className="mx-auto max-w-3xl">
+      <h1 className="mb-4 text-2xl font-bold text-[#f55]">
+        챔피언 상세 페이지 / SSR
+      </h1>
+
+      <h2 className="mb-4 text-4xl font-bold text-[#f55]">
+        {championsData.name}
+      </h2>
+      <h3 className="mb-4 text-2xl text-gray-600">{championsData.title}</h3>
+      <Image
+        src={`${API_BASE_URL}/cdn/15.5.1/img/champion/${championsData.image.full}`}
+        alt={championsData.name}
+        width={200}
+        height={200}
+        className="mx-auto"
+      />
+      <p className="mt-4 text-[#f55]">{championsData.blurb}</p>
+      <div className="mt-6">
+        <h3 className="text-xl font-semibold text-[#f55]">스탯</h3>
+        <ul className="list-inside list-disc">
+          <li>{`공격력 : ${championsData.info.attack}`}</li>
+          <li>{`방어력 : ${championsData.info.defense}`}</li>
+          <li>{`마법력 : ${championsData.info.magic}`}</li>
+          <li>{`난이도 : ${championsData.info.difficulty}`}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ChampionDetailIdPage;
