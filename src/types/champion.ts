3773891322dd8893c531f8dba97faf048c2f960e
Young <dndud5182@gmail.com>
2025-03-13 12:42:11 +0900

    Feat : 챔피언 데이터를 정의하는 타입 지정

---
+++ src/types/champion.ts
@@ -0,0 +1,12 @@
+// 챔피언 데이터를 정의하는 타입
+export interface ChampionData {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  image: {
+    full: string;
+  };
+  tags: string[];
+}
