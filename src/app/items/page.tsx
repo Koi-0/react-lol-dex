0df8f30a4ee4e71f36897939aeb5a0c9e1d16685
Young <dndud5182@gmail.com>
2025-03-17 02:49:55 +0900

    Fix : 중복된 key 문제 해결을 위해 index를 key로 추가

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -22,9 +22,9 @@ export default async function Itempage() {
     <div>
       <h1 className="mb-4 text-2xl font-bold text-[#f55]">아이템 목록 / SSG</h1>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
-        {items.map((item) => {
+        {items.map((item, index) => {
           return (
-            <div key={item.name} className="rounded border p-4 hover:shadow-lg">
+            <div key={index} className="rounded border p-4 hover:shadow-lg">
               <Image
                 src={`${API_BASE_URL}/cdn/15.5.1/img/item/${item.image.full}`}
                 alt={item.name}
