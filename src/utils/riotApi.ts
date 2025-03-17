1ffa4810596440f84c19176330947dbda03c52ff
Young <dndud5182@gmail.com>
2025-03-18 01:56:29 +0900

    Fix : 챔피언 로테이션 데이터 요청 시 중복 코드 정리 및 응답 처리 개선

--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -1,4 +1,4 @@
-// 챔피언 로테이션 데이터 가져오기 (클라이언트 로직)
+// 챔피언 로테이션 데이터 가져오기
 import { ChampionRotationData } from "@/types/championRotation";
 
 export const apiKey = process.env.NEXT_PUBLIC_RIOT_API_KEY;
@@ -12,14 +12,14 @@ export const getChampionRotationData =
       const response = await fetch(`${apiRequestUrl}?api_key=${apiKey}`);
 
       if (!response) {
-        // 응답 객체가 아예 존재하지 않는 경우 또는 fetch 자체가 실패한 경우
+        // fetch() 자체가 실패한 경우
         throw new Error("서버 응답이 없습니다.");
       } else if (!response.ok) {
-        // 응답 객체는 존재하지만 HTTP 상태 코드가 정상 범위(200~299)가 아닌 경우
+        // HTTP 상태 코드가 200~299 범위를 벗어난 경우
         throw new Error(`HTTP 오류 발생 : ${response.status}`);
       }
 
-      const data = await response.json();
+      const data: ChampionRotationData = await response.json();
 
       return data; // 챔피언 로테이션 데이터 반환
     } catch (error) {
