c2e2ef9f212c5f97634c3fcf92a68cb380d404ca
Young <dndud5182@gmail.com>
2025-03-18 15:27:45 +0900

    Chore : console.log 제거

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -12,13 +12,13 @@ export async function getLatestVersion(): Promise<string> {
   const response = await fetch(`${API_BASE_URL}/api/versions.json`);
   const data: string[] = await response.json();
 
-  return data[0]; // 가장 최신 버전 반환
+  return data[0];
 }
 
 // 최신 버전의 챔피언 데이터 가져오기
 export async function getChampionData(): Promise<ChampionData | null> {
   // API 요청 (ISR 적용)
-  const version = await getLatestVersion(); // 최신 버전 정보 가져오기
+  const version = await getLatestVersion();
   const championDetailUrl = `${API_BASE_URL}/cdn/${version}/data/ko_KR/champion.json`;
 
   try {
@@ -28,7 +28,7 @@ export async function getChampionData(): Promise<ChampionData | null> {
 
     const { data } = await response.json();
 
-    return data; // 챔피언 데이터 반환
+    return data;
   } catch (error) {
     console.log("챔피언 데이터를 불러오는 중 오류 발생 : ", error);
     return null;
@@ -40,7 +40,7 @@ export async function getChampionDetailData(
   id: string,
 ): Promise<ChampionData | null> {
   // API 요청 (SSR 적용)
-  const version = await getLatestVersion(); // 최신 버전 정보 가져오기
+  const version = await getLatestVersion();
   const championDetailUrl = `${API_BASE_URL}/cdn/${version}/data/ko_KR/champion/${id}.json`;
 
   try {
@@ -50,7 +50,7 @@ export async function getChampionDetailData(
 
     const { data } = await response.json();
 
-    return data[id]; // 챔피언 상세 데이터 반환
+    return data[id];
   } catch (error) {
     console.log("챔피언 상세 데이터를 불러오는 중 오류 발생 : ", error);
     return null;
@@ -60,7 +60,7 @@ export async function getChampionDetailData(
 // 최신 버전의 아이템 데이터 가져오기
 export async function getItemData(): Promise<ItemData | null> {
   // API 요청 (SSG 적용)
-  const version = await getLatestVersion(); // 최신 버전 정보 가져오기
+  const version = await getLatestVersion();
   const itemUrl = `${API_BASE_URL}/cdn/${version}/data/ko_KR/item.json`;
 
   try {
@@ -70,7 +70,7 @@ export async function getItemData(): Promise<ItemData | null> {
 
     const { data } = await response.json();
 
-    return data; // 아이템 데이터 반환
+    return data;
   } catch (error) {
     console.log("아이템 데이터를 불러오는 중 오류 발생 : ", error);
     return null;
