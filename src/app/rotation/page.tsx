25027e2633646c4f80ed1fb5b9f117e51a808f2d
Young <dndud5182@gmail.com>
2025-03-18 14:34:17 +0900

    Refactor : 챔피언 데이터 렌더링 방식 개선 Feat : 챔피언 상세 정보 및 이미지 추가 Style : 챔피언 리스트 UI 개선

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,36 +1,55 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
 import { getChampionRotationData } from "@/utils/riotApi";
+import { API_BASE_URL } from "@/utils/serverApi";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import Link from "next/link";
 
 const RotationPage = () => {
   // 챔피언 로테이션 데이터 가져오기
-  const { data, isLoading, error } = useQuery({
+  const {
+    data: freeChampionIds,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ["ChampionRotationData"],
     queryFn: getChampionRotationData,
   });
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>데이터를 불러오는 중 오류 발생</div>;
 
-  const champions = data
-    ? data.freeChampionIds.map((id) => ({
-        id,
-        name: `Champion ${id}`,
-        title: "Title Placeholder",
-      }))
-    : [];
+  if (error || !freeChampionIds)
+    return <div>데이터를 불러오는 중 오류 발생</div>;
 
   return (
     <div>
-      <h1>챔피언 로테이션</h1>
-      <div>
-        {champions?.map((champion) => (
-          <div key={champion.id}>
-            <h2>{champion.name}</h2>
-            <p>{champion.title}</p>
-          </div>
-        ))}
+      <h1 className="mb-4 text-2xl font-bold text-[#f55]">
+        챔피언 로테이션 / 이번주 무료로 플레이 할 수 있어요!
+      </h1>
+      <div className="grid grid-cols-4 gap-4">
+        {freeChampionIds.map((champion) => {
+          return (
+            <div
+              key={champion.id}
+              className="rounded border p-4 hover:shadow-lg"
+            >
+              <Link href={`/champions/${champion.id}`}>
+                <Image
+                  src={`${API_BASE_URL}/cdn/15.5.1/img/champion/${champion.image.full}`}
+                  alt={champion.name}
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />
+                <h2 className="mt-2 text-xl font-semibold text-[#f55]">
+                  {champion.name}
+                </h2>
+                <p className="text-gray-500">{champion.title}</p>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
